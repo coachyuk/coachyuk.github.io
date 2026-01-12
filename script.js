8d9f2718ceaff939936bab0e1f1ce87bf12a7abf
coachyuk <coachyuk@gmail.com>
2026-01-11 21:27:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showContactForm(){
+    document.getElementById("contact").style.display="block";
+}
+
+function hideContactForm(){
+    document.getElementById("contact").style.display="none";
+}
